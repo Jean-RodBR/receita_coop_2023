6df96951d2b6f4f87b099d1001f9a35c92df59ad
Jean Rod <101993941+Jean-RodBR@users.noreply.github.com>
2023-02-22 23:03:44 +0000

    "join on estabelecimentos e empresas"

--- script6_ajustando_tabelas.r
+++ script6_ajustando_tabelas.r
@@ -1,38 +1,21 @@
 library(dplyr)
+    #juntar esbelecimentos com empresas
 
-#transformar de acordo com o padrão
-#socios_cnpj_2023: cnpj, cnpj_basico, razao_social, 
-                    #identificador_de_socio, nome_socio, cnpj_cpf_socio, 
-                    #qualificacao_socio, data_entrada_sociedade, pais,
-                    # representante_legal, nome_representante, 
-                    #qualificacao_representante_legal, faixa_etaria
+    #importar dados da pasta dados_finais
+    estabelecimentos <- read.csv("/workspaces/receita_coop_2023/dados_finais/estabele.csv", sep = ",")
+    empresas <- read.csv("/workspaces/receita_coop_2023/dados_finais/empresas_filtradas.csv", sep = ",")
 
 
-#começando por socios------
-socios_2023 <- read.csv("dados_finais/socios_filtrados.csv", sep = ",")
-        
-    #retirar coluna data, alterar nome cnpj.cpf_socio para cnpj_cpf_socio, add cnpj_inteiro
-socios_2023 <- socios_2023 %>% 
-                    select(-data) %>% 
-                    rename(cnpj_cpf_socio = cnpj.cpf_socio)
+    #transformando ambas em tibbles
+    estabelecimentos <- tibble::as_tibble(estabelecimentos)
+    empresas <- tibble::as_tibble(empresas)
+
+    #juntando as tabelas com suffix
+    dados_cnpj_2023 <- 
+        left_join(estabelecimentos, empresas, 
+            by = "cnpj_basico", 
+            suffix = c("_estabelecimento", "_empresa"))
+
+    #salvando a tabela
+    write.csv(dados_cnpj_2023, "/workspaces/receita_coop_2023/dados_finais/dados_cnpj_2023.csv", row.names = TRUE)
     
-        
-
-
-
-
-#dados_cnpj_2023: cnpj, matriz_filial, razao_social, nome_fantasia,
-                    #situacao_cadastral, data_situacao_cadastral, motivo_situacao_cadastral, 
-                    #cnpj_basico, cnpj_ordem, cnpj_dv, nome_cidade_exterior,pais,data_inicio_atividades,
-                    #cnae_fiscal,cnae_fiscal_secundaria,tipo_logradouro,logradouro,numero,complemento,bairro,cep, 
-                    #uf,municipio, ddd1,telefone1,ddd2,telefone2,ddd_fax,fax,correio_eletronico,situacao_especial,
-                    #data_situacao_especial,natureza_juridica,qualificao_responsavel,porte_empresa,
-                    #ente_federativo_responsavel,capital_social,regiao,data,descricao_cnae_fiscal
-
-#cnae: cnpj, cnae_secundario, data, descricao_cnae_subclasse, descricao_cnae_divisao
-                    
-
-    
-
- 
-
